7ddd393d3e12d3c5222cb202078c4863762c7fa2
Ewgen <ewgen9826@list.ru>
2018-10-17 20:07:19 +0300

    Add Logic Section

--- CowRationWebApplication/Startup.cs
+++ CowRationWebApplication/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -22,7 +18,7 @@ namespace CowRationWebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var  connectionString=Configuration.GetConnectionString("DefaultConnection");
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
 
             services.AddDbContext<CowRationContext>(options =>
             options.UseSqlServer(connectionString));
